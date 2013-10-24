9db86bfa568a5f9483774faebbed77ac7ff98930
Geramy <admin@terratex.de>
2013-10-24 09:47:12 +0200

    BBCode Struktur erstellt

---
+++ com.geramy.wcf.hidehack.bbcode/files/lib/system/bbcode/hidehackBBCode.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace wcf\system\bbcode;
+use wcf\system\WCF;
+/**
+* Parses the progressBarBBCode bbcode tag.
+*
+* @package com.geramy.wcf.progressBarBBCode.bbcode
+* @copyright geramy
+* @author geramy (mit Hilfe von anderen BBcode Plugins aufgebaut)
+* @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+* @category WCF
+* @parameter
+*/
+class hidehackBBCode extends AbstractBBCode {
+    /**
+* @see wcf\system\bbcode\IBBCode::getParsedTag()
+*/
+    public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser) {
+
+
+
+        if ($parser->getOutputType() == 'text/html') {
+            WCF::getTPL()->assign(array('minimal' => false));
+            return WCF::getTPL()->fetch('hidehackBBCodeTag');
+        }
+        else{
+            WCF::getTPL()->assign(array('minimal' => true));
+            return WCF::getTPL()->fetch('hidehackBBCodeTag');
+        }
+
+
+
+    }
+}
